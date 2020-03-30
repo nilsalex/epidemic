e7d0f90b4746062fd0bf22e2fb6d8ca41ba74a1d
Nils Alex <nils@famalex.de>
2020-03-30 13:19:53 +0200

    progress

--- src/Lib.hs
+++ src/Lib.hs
@@ -25,6 +25,11 @@ data Population = Population
                   }
                   deriving Show
 
+data Model = Model
+             { history :: [(Int,Int,Int)]
+             , population :: Population
+             } deriving Show
+
 moveInd :: Double -> Individual -> Individual
 moveInd step (Individual x y xv yv)
     | inBounds x' && inBounds y' = Individual x' y' xv yv
@@ -56,6 +61,11 @@ randPop nSus nInf = do
   sus <- sequence (replicate nSus randInd)
   inf <- sequence (replicate nInf randInd)
   return $ Population sus (zip inf (repeat 0)) []
+
+randModel :: MonadRandom m => Int -> Int -> m Model
+randModel nSus nInf = do
+  p@(Population s i r) <- randPop nSus nInf
+  return $ Model [(length s,length i, length r)] p
 
 move :: Double -> Population -> Population
 move step (Population sus inf rem) = Population sus' inf' rem'
@@ -105,11 +115,12 @@ recover recov step (Population sus inf rem) = Population sus inf' rem'
                               else ((i,c'):infAcc, remAcc)) ([],rem) inf
 
 simulate' :: MonadRandom m =>
-             Double -> Double -> Double -> Double -> Population -> m Population
-simulate' ar r recov step p = do
+             Double -> Double -> Double -> Double -> Model -> m Model
+simulate' ar r recov step (Model h p) = do
   let p' = (recover recov step) p
-  p'' <- infect ar r step p'
-  return $ move step p''
+  p''@(Population sus inf rec) <- infect ar r step p'
+  let h' = h ++ [(length sus,length inf,length rec)]
+  return $ Model h' (move step p'')
 
 drawInd :: Individual -> Picture
 drawInd (Individual x y _ _) = translate x' y' $ circleSolid 3
@@ -122,12 +133,46 @@ drawPop (Population sus inf rem) = translate (-100) (-100) $ pictures [sus', inf
   where
     sus' = color blue $ pictures $ fmap drawInd sus
     inf' = color red $ pictures $ fmap (drawInd . fst) inf
-    rem' = color (greyN 0.5) $ pictures $ fmap drawInd rem
+    rem' = color (greyN 0.7) $ pictures $ fmap drawInd rem
+
+drawHist :: [(Int,Int,Int)] -> Picture
+drawHist xs@((s0,i0,r0):_) =
+    pictures
+    [ color red $ polygon pathI
+    , color blue $ polygon pathS
+    , color (greyN 0.7) $ polygon pathR
+    ]
+  where
+    (sl,il,rl) = last xs
+    n = s0+i0+r0
+    n' = fromIntegral n :: Float
+    l = length xs
+    l' = min 200 l
+    coords = if l <= 200
+             then zipWith (\(s,i,r) x -> (fromIntegral x, scale s, scale i, scale r)) xs [0..]
+             else let fac = (fromIntegral l) / 200
+                  in fmap (\ix -> let ix' = (round ((fromIntegral ix :: Double)*fac) :: Int)
+                                      (s,i,r) = xs !! ix'
+                                  in (fromIntegral ix, scale s, scale i, scale r)) [0..199]
+    pathI' = fmap (\(x,s,i,r) -> (x-100, i-200)) coords
+    pathS' = fmap (\(x,s,i,r) -> (x-100, i+s+1-200)) coords
+    pathR' = fmap (\(x,s,i,r) -> (x-100, i+s+r+2-200)) coords
+    pathI = (-100,-200) : fmap (\(x,s,i,r) -> (x-100, i-200)) coords ++ [((fromIntegral (l'-101)),-200)]
+    pathS = (-100, scale i0-200) : pathS' ++ [((fromIntegral (l'-101)), scale il-200)] ++ reverse pathI'
+    pathR = (-100, scale (i0+s0)+1-200) : pathR' ++ [((fromIntegral (l'-101)), scale (il+sl)+1-200)] ++ reverse pathS'
+
+    scale x = 70 * fromIntegral x / n'
+
+
+drawModel :: Model -> Picture
+drawModel (Model h p) = trace (show h) $ pictures [drawPop p,drawHist h]
 
 someFunc :: IO ()
 someFunc = do
-  pop <- evalRandIO (randPop 50 1)
   let attack = 0.2
   let radius = 0.05
   let recov  = 20
-  simulateIO (InWindow "epidemic" (640, 480) (0,0)) white 20 pop (pure . drawPop) (\_ t m -> simulate' attack radius recov (realToFrac t) m)
+  let s0 = 100
+  let i0 = 2
+  model <- evalRandIO (randModel s0 i0)
+  simulateIO (InWindow "epidemic" (640, 480) (0,0)) white 20 model (pure . drawModel) (\_ t m -> simulate' attack radius recov (realToFrac t) m)
